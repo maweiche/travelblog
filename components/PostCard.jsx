5fd00e1740cb283016b00cebee8c7244c6beb2f7
Matt Weichel <maweiche@gmail.com>
2022-03-15 12:08:47 -0400

    revert img tag

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -10,7 +10,7 @@ const PostCard = ({ post }) => {
   return (
     <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
       <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
-        <Image 
+        <img 
           src={post.featuredimage.url}
           alt={post.title}
           className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
@@ -23,7 +23,7 @@ const PostCard = ({ post }) => {
       </h1>
       <div className='block lg:flex text-center items-center justify-center mb-8 w-full'>
         <div className='flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8'>
-          <Image 
+          <img 
             alt={post.author.name}
             height='30px'
             width='30px'
